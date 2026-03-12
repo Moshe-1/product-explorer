2bd8a2849cb4e128a81dbf427c7350a73ee40bef
mlesejane <mlesejane@mobiquityinc.com>
2026-03-12 20:19:18 +0200

    Feature: Implemented catalog view with search and modern template control flow also counter of products. Loading state UI, Success state UI and error state UI

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../../core/services/product.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
 })
-export class CatalogComponent {}
+export class CatalogComponent implements OnInit {
+  protected productService = inject(ProductService);
+
+  ngOnInit() {
+    this.productService.loadProducts();
+  }
+
+  updateSearch(event: Event) {
+    const term = (event.target as HTMLInputElement).value;
+    this.productService.filter.update((f) => ({ ...f, search: term }));
+  }
+}
