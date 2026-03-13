c5fae7751ae1b29dd26ffb5a703b2499a4b4f973
mlesejane <mlesejane@mobiquityinc.com>
2026-03-13 08:55:23 +0200

    Feature: Created favorites view with reactive signal filtering and empty-state handling using flow controls

--- src/app/features/favorites/favorites.component.ts
+++ src/app/features/favorites/favorites.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, computed } from '@angular/core';
+import { ProductService } from '../../core/services/product.service';
+import { FavoritesService } from '../../core/services/favorites.service';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../core/models/product.model';
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  standalone: true,
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
 })
-export class FavoritesComponent {}
+export class FavoritesComponent {
+  private productService = inject(ProductService);
+  protected fav = inject(FavoritesService);
+
+  favoriteProducts = computed(() => {
+    const favIds = this.fav.ids();
+    return this.productService.filteredProducts().filter((p: Product) => favIds.includes(p.id));
+  });
+}
