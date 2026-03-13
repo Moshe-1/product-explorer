2a9199bb9c53efb41dba17826a6c9be26ca9a3be
mlesejane <mlesejane@mobiquityinc.com>
2026-03-13 08:29:43 +0200

    Feature: implement reactive favorites service with localStorage sync and parameterized details view

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, computed, signal } from '@angular/core';
+import { ProductService } from '../../core/services/product.service';
+import { FavoritesService } from '../../core/services/favorites.service';
+import { CurrencyPipe } from '@angular/common';
+import { Product } from '../../core/models/product.model';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  standalone: true,
+  imports: [CurrencyPipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent {
+  private productService = inject(ProductService);
+  protected fav = inject(FavoritesService);
+  private productId = signal<string>('');
+  // Router passes 'id'  withComponentInputBinding  configured in app config
+  @Input() set id(value: string) {
+    this.productId.set(value);
+  }
+
+  product = computed(() =>
+    this.productService.filteredProducts().find((p: Product) => p.id === this.productId()),
+  );
+}
